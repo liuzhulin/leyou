daa3ea61e9825991ec34a2f9e3f5fd3552ac30fa
liuzhulin <1392673190@qq.com>
2020-01-11 20:32:05 +0800

    增加品牌管理分页查询功能

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -0,0 +1,45 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.pojo.Brand;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class BrandService {
+    @Autowired
+    BrandMapper brandMapper;
+
+    /**
+     * 根据品牌条件分页查询品牌信息
+     * @param key
+     * @param page
+     * @param rows
+     * @param sortBy
+     * @param desc
+     * @return
+     */
+    public PageResult<Brand> queryBrandsByPage(String key, Integer page, Integer rows, String sortBy, Boolean desc) {
+
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("name", "%" + key + "%").orEqualTo("letter", key);
+        }
+        PageHelper.startPage(page, rows);
+        if (StringUtils.isNotBlank(sortBy)) {
+            example.setOrderByClause(sortBy + " " + (desc ? "desc" : "asc"));
+        }
+        List<Brand> brands = brandMapper.selectByExample(example);
+        PageInfo<Brand> pageInfo = new PageInfo<>(brands);
+        return new PageResult<>(pageInfo.getTotal(), pageInfo.getPages(), pageInfo.getList());
+    }
+}
